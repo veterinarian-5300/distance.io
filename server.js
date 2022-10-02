9b40d24d2acbc76dc8265a49b09a39bb21cc8e42
veterinarian-5300 <anmolarora5300@gmail.com>
2022-10-02 13:18:54 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+mapboxgl.accessToken =  "pk.eyJ1IjoiYW5tb2xhcm9yYTUzMDAiLCJhIjoiY2w4cXM5MmdwMDRidDN2bnV2bGlpYXBhcyJ9.Nf2GkCwiVsyrD1sDtSdBJg"
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccuracy: true
+})
+
+function successLocation(position) {
+  setupMap([position.coords.longitude, position.coords.latitude])
+}
+
+function errorLocation() {
+  setupMap([-2.24, 53.48])
+}
+
+function setupMap(center) {
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: center,
+    zoom: 8
+  })
+
+  const nav = new mapboxgl.NavigationControl()
+  map.addControl(nav)
+
+  var directions = new MapboxDirections({
+    accessToken: mapboxgl.accessToken
+  })
+
+  map.addControl(directions, "top-left")
+}
